9a3a2282ef88a3ba9deb33a7735f1aba1ec24e88
Wagner Araujo <wagnercorp17@gmail.com>
2024-09-16 19:01:04 -0300

    3 usos de useEffect explicados

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,22 @@ const App = () => {
   const [contar, setContar] = React.useState(0);
 
   console.log('Executou Fora!!');
+  // useEffect é uma funcao que contem dependencias '[]', sem dependencias o cotigo em seu interior é sempre utilizado toda vez q a mesma é chamada
   React.useEffect(() => {
-    console.log('useEffect');
+    console.log('useEffect sempre!!');
+  });
+
+  // Com uma dependencia vazia o cotigo em seu interior é executado apenas 1 vez, na renderização da pagina
+  React.useEffect(() => {
+    console.log('useEffect apenas 1 vez');
   }, []);
+
+  // Com uma dependencia a funcao sera executada sempre que ouver uma mudança na dependencia
+  React.useEffect(() => {
+    console.log('useEffect com dependencia');
+    document.title = 'UseEffect' + contar;
+  }, [contar]);
+
   return <button onClick={() => setContar(contar + 1)}>{contar}</button>;
 };
 
