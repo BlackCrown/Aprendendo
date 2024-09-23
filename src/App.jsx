eb5da9b4e798ffac796d132a038a8a1320b3bf73
Wagner Araujo <wagnercorp17@gmail.com>
2024-09-23 18:47:30 -0300

    Exercicio finalizado e comentado, todo o site funcionando normalmente!!

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Produto from './Produto';
+import Produto from './produto';
 
 // Quando o usuário clicar em um dos botões, faça um fetch do produto clicado utilizando a api abaixo
 // https://ranekapi.origamid.dev/json/api/produto/notebook
@@ -9,16 +9,23 @@ import Produto from './Produto';
 // Quando o usuário entrar no site, se existe um produto no localStorage, faça o fetch do mesmo
 
 const App = () => {
+  // Definido o state, o mesmo recebe dados dos botoes da pagina e armazena
   const [produto, setProduto] = React.useState(null);
+
+  // useEffect ira rodar ao iniciar a pagina
   React.useEffect(() => {
+    // Ao abrir a pagina é salvo os dados do localStorage
     const produtoLocal = window.localStorage.getItem('produto');
     if (produto !== 'null') {
+      // caso localStorage nao esteja vazio, o dado é salvo como produto
       setProduto(produtoLocal);
     }
   }, []);
 
+  // Sempre q o usuario clica nos botoes a var 'produto' é modificada, ativando assim este useEffect
   React.useEffect(() => {
     if (produto !== 'null') {
+      // Ao ser ativada a funcao salva a ultima escolha do usuário no localStorage
       window.localStorage.setItem('produto', produto);
     }
   }, [produto]);
@@ -28,11 +35,13 @@ const App = () => {
     if (produto) localStorage.setItem('produto', produto);
   }
 
+  /* A escolha de produto do usuario é enviada ao arquivo produto com uso de props*/
   return (
     <>
       <h1>Preferência: {produto}</h1>
       <button onClick={handleClick}>notebook</button>
       <button onClick={handleClick}>smartphone</button>
+
       <Produto produto={produto} />
     </>
   );
