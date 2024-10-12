ad7af392ce691b8639a05a7b7955af01d9f9f97f
Wagner Araujo <wagnercorp17@gmail.com>
2024-10-12 12:22:50 -0300

    exercicio solucionado com uso de map function no array

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,31 @@ import Produto from './Produto';
 // Utilizando a array abaixo para mostrar
 // cada checkbox na tela.
 
-const coresArray = ['azul', 'roxo', 'laranja', 'verde', 'vermelho', 'cinza'];
-
 const App = () => {
-  return <></>;
+  const [cores, setCores] = React.useState([]);
+  const coresArray = ['azul', 'roxo', 'laranja', 'verde', 'vermelho', 'cinza'];
+
+  function handleChange({ target }) {
+    if (cores.includes(target.value)) {
+      setCores(cores.filter((cor) => cor !== target.value));
+    } else {
+      setCores([...cores, target.value]);
+    }
+    console.log(cores);
+  }
+
+  return (
+    <>
+      <form>
+        {coresArray.map((cor) => (
+          <label key={cor}>
+            <input type="checkbox" value={cor} onChange={handleChange} />
+            {cor}
+          </label>
+        ))}
+      </form>
+    </>
+  );
 };
 
 export default App;
