7950afd3e4649522b30d46db81f4116d22ecf129
Wagner Araujo <wagnercorp17@gmail.com>
2024-10-12 17:58:48 -0300

    Radio component feito e funcionando

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,28 @@
 import React from 'react';
 import Input from './Form/Input';
 import Select from './Form/Select';
+import Radio from './Form/Radio';
 
 const App = () => {
   const [nome, setNome] = React.useState('');
   const [email, setEmail] = React.useState('');
   const [value, setValue] = React.useState('');
+  const [cor, setCor] = React.useState('');
 
   return (
     <>
+      <Radio options={['Azul', 'Vermelho']} value={cor} setValue={setCor} />
+
       <Select
         options={['smartphone', 'notebook']}
         value={value}
         setValue={setValue}
       />
+
       <Input id="nome" label="nome" value={nome} setValue={setNome} required />
       <Input id="email" label="Email" value={email} setValue={setEmail} />
+
+      <button>Enviar</button>
     </>
   );
 };
