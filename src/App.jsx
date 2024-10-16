2874afaad4cde91d603b673d00f181341a7aa921
Wagner Araujo <wagnercorp17@gmail.com>
2024-10-15 22:02:10 -0300

    Exercicio de Forms resolvido com uso de react components e useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,96 @@
 import React from 'react';
+import useForm from './Hooks/useForm';
+import Radio from './Form/Radio';
 
-function App() {
-  return <div>App</div>;
-}
+const App = () => {
+  const [resposta, setResposta] = React.useState('');
+  const [score, setScore] = React.useState(0);
+  const [perguntaNum, setPerguntaNum] = React.useState(0);
+  const [erro, setErro] = React.useState(null);
+
+  const perguntas = [
+    {
+      pergunta: 'Qual método é utilizado para criar componentes?',
+      options: [
+        'React.makeComponent()',
+        'React.createComponent()',
+        'React.createElement()',
+      ],
+      resposta: 'React.createElement()',
+      id: 'p1',
+    },
+    {
+      pergunta: 'Como importamos um componente externo?',
+      options: [
+        'import Component from "./Component"',
+        'require("./Component")',
+        'import "./Component"',
+      ],
+      resposta: 'import Component from "./Component"',
+      id: 'p2',
+    },
+    {
+      pergunta: 'Qual hook não é nativo?',
+      options: ['useEffect()', 'useFetch()', 'useCallback()'],
+      resposta: 'useFetch()',
+      id: 'p3',
+    },
+    {
+      pergunta: 'Qual palavra deve ser utilizada para criarmos um hook?',
+      options: ['set', 'get', 'use'],
+      resposta: 'use',
+      id: 'p4',
+    },
+  ];
+
+  function checarResposta() {
+    const resultado = resposta === perguntas[perguntaNum].resposta;
+    if (resultado) {
+      setScore((score) => score + 1);
+      setPerguntaNum((perguntaNum) => perguntaNum + 1);
+      setResposta('');
+    } else {
+      setPerguntaNum((perguntaNum) => perguntaNum + 1);
+      setResposta('');
+    }
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    if (resposta !== '') {
+      checarResposta();
+      setErro(null);
+    } else {
+      console.log('Selecione uma resposta!');
+      setErro('Selecione uma resposta!!');
+    }
+  }
+
+  return (
+    <>
+      {perguntaNum < perguntas.length && (
+        <form onSubmit={handleSubmit}>
+          <h1>Pergunta {perguntaNum + 1}</h1>
+          <p>{perguntas[perguntaNum].pergunta}</p>
+          <Radio
+            options={perguntas[perguntaNum].options}
+            value={resposta}
+            // checked={resposta.includes(resposta)}
+            setValue={setResposta}
+          />
+          <p>{erro}</p>
+
+          <button>Próxima</button>
+        </form>
+      )}
+      {perguntaNum > perguntas.length - 1 && (
+        <div>
+          <h1>Sua pontuação</h1>
+          <p>{score}</p>
+        </div>
+      )}
+    </>
+  );
+};
 
 export default App;
--- src/Hooks/useForm.jsx
+++ src/Hooks/useForm.jsx
@@ -1,7 +1,47 @@
 import React from 'react';
 
-function useForm() {
-  return <div>useForm</div>;
-}
+const types = {
+  cep: {
+    regex: /^\d{5}-?\d{3}$/,
+    message: 'CEP inválido',
+  },
+  email: {
+    regex: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+    message: 'Email inválido',
+  },
+};
+
+const useForm = (type) => {
+  const [value, setValue] = React.useState('');
+  const [error, setError] = React.useState(null);
+
+  function validate(value) {
+    if (type === false) return true;
+    if (value.length === 0) {
+      setError('Preencha um valor');
+      return false;
+    } else if (types[type] && !types[type].regex.test(value)) {
+      setError(types[type].message);
+      return false;
+    } else {
+      setError(null);
+      return true;
+    }
+  }
+
+  function onChange({ target }) {
+    if (error) validate(target.value);
+    setValue(target.value);
+  }
+
+  return {
+    value,
+    setValue,
+    error,
+    onChange,
+    onBlur: () => validate(value),
+    validate: () => validate(value),
+  };
+};
 
 export default useForm;
