0e7f8d542b7c42940064d06a392f875e29aa10e5
Wagner Araujo <wagnercorp17@gmail.com>
2024-10-18 20:40:15 -0300

    Exemplos de uso do styled components com comentarios para ajuda futura

--- src/App.jsx
+++ src/App.jsx
@@ -1,96 +1,146 @@
-import React from 'react';
-import useForm from './Hooks/useForm';
-import Radio from './Form/Radio';
+// Lembre-se de importar o styled component e fazer sua instalação pelo NPM
+/* npm install styled-components */
+import styled from 'styled-components';
+//Faça instalação tbm do plugin do VScode, para ter autocomplet no seu código
+/* vscode-styled-components */
+
+// const Title = styled.h1`
+//   font-size: 1.5em;
+//   color: tomato;
+// `;
+
+// const App = () => {
+//   return (
+//     <div>
+//       <Title>Título principal</Title>
+//     </div>
+//   );
+// };
+
+const ProdutosContainer = styled.div`
+  display: flex;
+`;
+
+const Produto = styled.div`
+  flex: 1;
+`;
+const Titulo = styled.h1`
+  font-size: 2em;
+`;
+
+const Comprar = styled.button`
+  font-size: 1.5em;
+  background: transparent;
+  padding: 0.5rem;
+  border-radius: 4px;
+  border: 2px solid;
+  cursor: pointer;
+`;
+
+const Preco = styled.span`
+  background: #53d956;
+  color: white;
+  display: inline-block;
+  border-radius: 5px;
+  padding: 0.5rem;
+`;
 
 const App = () => {
-  const [resposta, setResposta] = React.useState('');
-  const [score, setScore] = React.useState(0);
-  const [perguntaNum, setPerguntaNum] = React.useState(0);
-  const [erro, setErro] = React.useState(null);
-
-  const perguntas = [
-    {
-      pergunta: 'Qual método é utilizado para criar componentes?',
-      options: [
-        'React.makeComponent()',
-        'React.createComponent()',
-        'React.createElement()',
-      ],
-      resposta: 'React.createElement()',
-      id: 'p1',
-    },
-    {
-      pergunta: 'Como importamos um componente externo?',
-      options: [
-        'import Component from "./Component"',
-        'require("./Component")',
-        'import "./Component"',
-      ],
-      resposta: 'import Component from "./Component"',
-      id: 'p2',
-    },
-    {
-      pergunta: 'Qual hook não é nativo?',
-      options: ['useEffect()', 'useFetch()', 'useCallback()'],
-      resposta: 'useFetch()',
-      id: 'p3',
-    },
-    {
-      pergunta: 'Qual palavra deve ser utilizada para criarmos um hook?',
-      options: ['set', 'get', 'use'],
-      resposta: 'use',
-      id: 'p4',
-    },
-  ];
-
-  function checarResposta() {
-    const resultado = resposta === perguntas[perguntaNum].resposta;
-    if (resultado) {
-      setScore((score) => score + 1);
-      setPerguntaNum((perguntaNum) => perguntaNum + 1);
-      setResposta('');
-    } else {
-      setPerguntaNum((perguntaNum) => perguntaNum + 1);
-      setResposta('');
-    }
-  }
-
-  function handleSubmit(event) {
-    event.preventDefault();
-    if (resposta !== '') {
-      checarResposta();
-      setErro(null);
-    } else {
-      console.log('Selecione uma resposta!');
-      setErro('Selecione uma resposta!!');
-    }
-  }
-
   return (
-    <>
-      {perguntaNum < perguntas.length && (
-        <form onSubmit={handleSubmit}>
-          <h1>Pergunta {perguntaNum + 1}</h1>
-          <p>{perguntas[perguntaNum].pergunta}</p>
-          <Radio
-            options={perguntas[perguntaNum].options}
-            value={resposta}
-            // checked={resposta.includes(resposta)}
-            setValue={setResposta}
-          />
-          <p>{erro}</p>
-
-          <button>Próxima</button>
-        </form>
-      )}
-      {perguntaNum > perguntas.length - 1 && (
-        <div>
-          <h1>Sua pontuação</h1>
-          <p>{score}</p>
-        </div>
-      )}
-    </>
+    // O seu código CSS será invocado como um componente, como demonstrado abaixo...
+    <ProdutosContainer>
+      <Produto>
+        <Titulo>
+          Notebook <Preco>R$ 1999</Preco>
+        </Titulo>
+        <Comprar>Comprar</Comprar>
+      </Produto>
+      <Produto>
+        <Titulo>
+          Smartphone <Preco>R$ 2999</Preco>
+        </Titulo>
+        <Comprar>Comprar</Comprar>
+      </Produto>
+    </ProdutosContainer>
   );
 };
 
-export default App;
+/* #Region PROPS */
+//Usando Javascript podem ser feitas alterações no seu css, com uso de ternarios, ifs e outros codigos Javascript
+// const Preco = styled.p`
+//   background: ${(props) => props.cor};
+//   color: white;
+//   display: inline-block;
+//   border-radius: 5px;
+//   padding: 0.5rem;
+//   `;
+
+//   const App = () => {
+//     function template(value, total) {
+//       console.log(value);
+//       console.log(total);
+//     }
+//     const total = 10;
+//     template`São ${total} no total`;
+
+//     return (
+//       <div>
+//         <Preco cor="#53d956">R$ 2999</Preco>
+//         <Preco cor="#84e">R$ 1999</Preco>
+//       </div>
+//     );
+//   };
+/* #EndRegion PROPS */
+
+/* #Region State*/
+//Pode ser feito uso também de States, definições de estado para uso no CSS através do Javascript como demonstrado abaixo
+// import styled from 'styled-components';
+
+// const Button = styled.button`
+//   background: ${({ ativo }) => (ativo ? '#53d956' : '#000')};
+//   border: 1px solid black;
+//   font-size: 1rem;
+//   padding: 0.5rem;
+//   border-radius: 4px;
+//   color: white;
+//   cursor: pointer;
+// `;
+
+// const App = () => {
+//   const [ativo, setAtivo] = React.useState(false);
+
+//   return (
+//     <Button ativo={ativo} onClick={() => setAtivo(!ativo)}>
+//       Ativar
+//     </Button>
+//   );
+// };
+/* #EndRegion State*/
+
+/* #Region Pseudo*/
+//O uso de pseudo elements é feito com & seguido do pseudo e seu código entre {}
+// const Comprar = styled.button`
+// font-size: 1.5em;
+// background: transparent;
+// padding: 0.5rem;
+// border-radius: 4px;
+// border: 2px solid black;
+// cursor: pointer;
+// position: relative;
+// &:hover {
+//   background: black;
+//   color: white;
+// }
+// &::before {
+//   display: block;
+//   content: '';
+//   width: 16px;
+//   height: 16px;
+//   border-radius: 50%;
+//   position: absolute;
+//   background: #53d956;
+//   top: -8px;
+//   right: -8px;
+// }
+// `;
+/* #EndRegion */
